558dc91963805dff300fd6c74072134e7d2f7f7c
mprimo <mprimo@ext.sancorseguros.com>
2022-07-11 16:11:55 -0300

    [NEW]: user model and message model

---
+++ Model/MessageModel.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+
+
+
+const MessageSchema = Schema({
+
+    from: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    
+    to: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    message: {
+        type: String,
+        required: true
+    }
+
+},  {
+    timestamps: true
+    }
+)
+
+
+
+MessageSchema.methods.toJSON = function () {
+    const { __v, ...object } = this.toObject();
+    return object;
+}
+
+
+
+module.exports = model("Message", MessageSchema);
